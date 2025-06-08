7bdfd1e43347f71c64132d7d1d877e41475ada35
AIT TGHRAYT Youssef <tghrayt.ysf@gmail.com>
2025-06-08 20:16:43 +0200

    upgrade de l'application vers 16

--- MiniStore-Angular/src/app/helper/admin.guard.ts
+++ MiniStore-Angular/src/app/helper/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 
 
@@ -8,7 +8,7 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 @Injectable({
     providedIn: 'root'
   })
-  export class AdminGuard implements CanActivate {
+  export class AdminGuard  {
   
   constructor (private router : Router) {} 
     canActivate(
